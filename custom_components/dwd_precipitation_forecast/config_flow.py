d9f35f932388b0f0a8d00f22bf6b99951f177d83
stoppegp <ich@stoppe-gp.de>
2024-06-18 17:48:05 +0200

    add translations

--- custom_components/dwd_precipitation_forecast/config_flow.py
+++ custom_components/dwd_precipitation_forecast/config_flow.py
@@ -15,8 +15,7 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-# TODO adjust the data schema to the data that you need
-STEP_USER_DATA_SCHEMA = vol.Schema(
+STEP_LOCATION_DATA_SCHEMA = vol.Schema(
     {
         vol.Required("name"): str,
         vol.Required("x"): float,
@@ -67,7 +66,7 @@ class ConfigFlow(ConfigFlow, domain=DOMAIN):
                 return self.async_create_entry(title=info["title"], data=user_input)
 
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="user", data_schema=STEP_LOCATION_DATA_SCHEMA, errors=errors
         )
 
 
